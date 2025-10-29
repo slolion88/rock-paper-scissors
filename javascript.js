92147bda7ea486de5cc9326d0666a47219d2424a
SloLion <237851181+slolion88@users.noreply.github.com>
2025-10-29 17:47:21 -0500

    Add Javascript update
    Forgot to add Javascript updates in the last commit, added here.

--- javascript.js
+++ javascript.js
@@ -1,117 +1,255 @@
-// Rock Paper Scissors game, computer vs user, tracking scores for both
+function resetScreen() {
+    const content = document.querySelector(".content");
+    const footer = document.querySelector(".footer");
 
-// Function to normalize the input received from the user
-// Remove input string's whitespace and convert to all lower case
-// Capitallize the first letter, separate from the initial string, then concatenate
-// Returns string with no whitespace and first letter capitalized
-function normalizeInput(input) {
-    // Remove whitespace from input and make all lower case
-    input = input.trim();
-    input = input.toLowerCase();
-    
-    // Get the first letter and capitalize it
-    let firstLetter = input.charAt(0);
-    firstLetter = firstLetter.toUpperCase();
-    
-    // Remove the first letter from the final string, then concatenate with uppercase first letter
-    input = input.substring(1);
-    return firstLetter.concat(input);
-}
-
-// Function to confirm the user's choice 
-// If no valid choice given, reprompt until vallid choice received
-// Returns the user's choice of Rock, Paper, or Scissors
-function confirmUserChoice(choice) {
-    choice = normalizeInput(choice);
-
-    while (choice != "Rock" && choice != "Paper" && choice != "Scissors") {
-        choice = prompt("Invalid entry, try again. Which do you choose? Enter \"rock\", \"paper\" or \"scissors\": ");
-        choice = normalizeInput(choice);
+    while (content.firstElementChild) {
+        content.removeChild(content.firstElementChild);
     }
 
-    return choice;
-}
-
-// Function to get computer's choice
-// Get a random number (0-1) as multiple of 10, rounded down. Gives an integer, 0-9.
-// If 0, will loop until another number received. 1-3 is Rock, 4-6 is Paper, 7-9 is Scissors
-// Returns the computer's choice of Rock, Paper, or Scissors
-function getComputerChoice() {
-    let randomNumber = 0; 
-    while (randomNumber === 0) {
-        randomNumber = Math.floor(Math.random() * 10);
-
-        switch (randomNumber) {
-            case 1: 
-            case 2: 
-            case 3:
-                return "Rock";
-            case 4:
-            case 5:
-            case 6:
-                return "Paper";
-            case 7: 
-            case 8: 
-            case 9:
-                return "Scissors";
-        }
+    while (footer.firstElementChild) {
+        footer.removeChild(footer.firstElementChild);
     }
+
+    footer.textContent = "";
 }
 
-// Function to determine the round winner
-// Rock ties with Rock, loses to Paper, beats Scissors
-// Paper beats Rock, ties with Paper, loses to Scissors
-// Scissors loses to Rock, beats Paper, ties with Scissors
-function getRoundWinner(user, computer) {
-     if (user === "Rock") {
-        return computer === "Rock" ? "It was a tie! Good game!" : (computer === "Paper" ? "The Computer. Womp womp." : "You! You win!");
-    } else if (user === "Paper") {
-        return computer === "Rock" ? "You! You win!" : (computer === "Paper" ? "It was a tie! Good game!" : "The Computer. Womp womp.");
+function displaySelection(scores) {
+    resetScreen();
+
+    // Reinitializing the player and computer selections
+    scores[0][0] = scores[1][0] = "";
+
+    const content = document.querySelector(".content");
+    const footer = document.querySelector(".footer");
+
+    // Adding the title
+    const title = document.createElement("h1");
+    title.textContent = "Choose your weapon!";
+    content.appendChild(title);
+
+    // Adding a section of options to choose from
+    const selections = document.createElement("div");
+    selections.className = "selections";
+    content.appendChild(selections);
+
+    // ROCK
+    const rockContainer = document.createElement("div");
+    rockContainer.id = "rock";
+    selections.appendChild(rockContainer);
+
+    const rockImage = document.createElement("img");
+    rockImage.src = "images/rock.png";
+    rockImage.alt = "Rock";
+    rockContainer.appendChild(rockImage);
+
+    const rockButton = document.createElement("button");
+    rockButton.textContent = "Rock";
+    rockContainer.appendChild(rockButton);
+
+    // PAPER
+    const paperContainer = document.createElement("div");
+    paperContainer.id = "paper";
+    selections.appendChild(paperContainer);
+
+    const paperImage = document.createElement("img");
+    paperImage.src = "images/paper.png";
+    paperImage.alt = "Paper";
+    paperContainer.appendChild(paperImage);
+
+    const paperButton = document.createElement("button");
+    paperButton.textContent = "Paper";
+    paperContainer.appendChild(paperButton);
+
+    // SCISSORS
+    const scissorsContainer = document.createElement("div");
+    scissorsContainer.id = "scissors";
+    selections.appendChild(scissorsContainer);
+
+    const scissorsImage = document.createElement("img");
+    scissorsImage.src = "images/scissors.png";
+    scissorsImage.slt = "Scissors";
+    scissorsContainer.appendChild(scissorsImage);
+
+    const scissorsButton = document.createElement("button");
+    scissorsButton.textContent = "Scissors";
+    scissorsContainer.appendChild(scissorsButton);
+
+    // Update the footer to display the current player and computer score.
+    footer.setAttribute("id", "score");
+
+    const playerScore = document.createElement("div");
+    playerScore.id = "score player";
+    playerScore.textContent = "Your Score: " + scores[0][1];
+    footer.appendChild(playerScore);
+
+    const computerScore = document.createElement("div");
+    computerScore.id = "score computer";
+    computerScore.textContent = "Computer Score: " + scores[1][1];
+    footer.appendChild(computerScore);
+}
+
+function displayResults(scores) {
+    resetScreen();
+
+    getComputerSelection(scores);
+    const playerSelection = scores[0][0];
+    const computerSelection = scores[1][0];
+
+    const winner = getRoundWinner(scores);
+    const playerScore = scores[0][1];
+    const computerScore = scores[1][1];
+
+    const content = document.querySelector(".content");
+
+    // Display the results - Image, Selection and Score under each player.
+    const results = document.createElement("div");
+    results.className = "results";
+    content.appendChild(results);
+
+    // PLAYER
+    const playerDetails = document.createElement("div");
+    playerDetails.id = "player";
+    results.appendChild(playerDetails);
+
+    const playerSelectionImage = document.createElement("img");
+    if (playerSelection === "Rock") {
+        playerSelectionImage.src = "images/rock.png";
+    } else if (playerSelection === "Paper") {
+        playerSelectionImage.src = "images/paper.png";
+    } else if (playerSelection === "Scissors") {
+        playerSelectionImage.src = "images/scissors.png";
+    }
+    playerDetails.appendChild(playerSelectionImage);
+
+    const playerTitle = document.createElement("div");
+    playerTitle.textContent = "Player:";
+    playerTitle.id = "player-title";
+    playerDetails.appendChild(playerTitle);
+
+    const playerSelectionText = document.createElement("div");
+    playerSelectionText.textContent = "Selection: " + playerSelection;
+    playerDetails.appendChild(playerSelectionText);
+
+    const playerScoreText = document.createElement("div");
+    playerScoreText.textContent = "Score: " + playerScore;
+    playerDetails.appendChild(playerScoreText);
+
+    // COMPUTER
+    const computerDetails = document.createElement("div");
+    computerDetails.id = "computer";
+    results.appendChild(computerDetails);
+
+    const computerSelectionImage = document.createElement("img");
+    if (computerSelection === "Rock") {
+        computerSelectionImage.src = "images/rock.png";
+    } else if (computerSelection === "Paper") {
+        computerSelectionImage.src = "images/paper.png";
+    } else if (computerSelection === "Scissors") {
+        computerSelectionImage.src = "images/scissors.png";
+    }
+    computerDetails.appendChild(computerSelectionImage);
+
+    const computerTitle = document.createElement("div");
+    computerTitle.textContent = "Computer:";
+    computerTitle.id = "computer-title";
+    computerDetails.appendChild(computerTitle);
+
+    const computerSelectionText = document.createElement("div");
+    computerSelectionText.textContent = "Selection: " + computerSelection;
+    computerDetails.appendChild(computerSelectionText);
+
+    const computerScoreText = document.createElement("div");
+    computerScoreText.textContent = "Score: " + computerScore;
+    computerDetails.appendChild(computerScoreText);
+
+    // Add h2 and h1 to display winner.
+    const preHeader = document.createElement("h2");
+    preHeader.textContent = "And the winner is...";
+    content.appendChild(preHeader);
+
+    const header = document.createElement("h1");
+    if (winner === "player") {
+        header.textContent = "You are! You win!"
+    } else if (winner === "computer") {
+        header.textContent = "The computer... womp womp."
     } else {
-        return computer === "Rock" ? "The Computer. Womp womp." : (computer === "Paper" ? "You! You win!" : "It was a tie! Good game!");
+        header.textContent = "It's a tie! Good game!";
     }
+    content.appendChild(header);
+
+    // Add replay button
+    const replayButton = document.createElement("button");
+    replayButton.id = "replay-button";
+    replayButton.textContent = "Play again?";
+    replayButton.addEventListener("click", () => {
+        startGame(scores);
+    });
+    content.appendChild(replayButton);
+
+    // Reformat footer
+    const footer = document.querySelector(".footer");
+    footer.textContent = "Thank you for playing!";
+    footer.id = "results";
 }
 
-// Initialize global variables, getting the user's choice. 
-let userScore = computerScore = 0;
-let userChoice = prompt(`
-    Let's play Rock, Paper, Scissors! Which do you choose? 
-    Type \"rock\", \"paper\", or \"scissors\" below: `
-);
-userChoice = confirmUserChoice(userChoice);
+function getPlayerSelection(scores) {
+    const rockButton = document.querySelector("#rock");
+    const paperButton = document.querySelector("#paper");
+    const scissorsButton = document.querySelector("#scissors");
 
-// Loops until the users advises they don't want to play anymore
-while (userChoice != null) {
-    // Get computer's choice
-    let computerChoice = getComputerChoice();
+    rockButton.addEventListener("click", () => {
+        scores[0][0] = "Rock";
+        displayResults(scores);
+    });
+    paperButton.addEventListener("click", () => {
+        scores[0][0] = "Paper";
+        displayResults(scores);
 
-    // Determine the winner and increase their score - Rock beats scissors, scissors beats paper, paper beats rock
-    let roundWinner = getRoundWinner(userChoice, computerChoice);
+    });
+    scissorsButton.addEventListener("click", () => {
+        scores[0][0] = "Scissors";
+        displayResults(scores);
+    });
+}
 
-    // Increase the winner's score
-    if (roundWinner === "You! You win!") {
-        userScore = ++userScore;
-    } else if (roundWinner === "The Computer. Womp womp.") {
-        computerScore = ++computerScore;
-    }
+// Randomizes computer's selection between 1, 2 and 3 as Rock, Paper and Scissors respectively.
+function getComputerSelection(scores) {
+    const randomNumber = Math.floor(Math.random() * 3);
+    scores[1][0] = randomNumber == 1 ? "Rock" : randomNumber == 2 ? "Paper" : "Scissors";
+}
 
-    // Display who won, what the current score is, and prompt to play again
-    userChoice = prompt(`
-        You chose: ${userChoice}
-        Computer chose: ${computerChoice}
-        The winner is... ${roundWinner}
-        Your score: ${userScore}
-        Computer's score: ${computerScore}
-        To play again, type \"rock\", \"paper\", or 
-        \"scissors\" below :`
-    );
+function getRoundWinner(scores) {
+    let playerSelection = scores[0][0];
+    let computerSelection = scores[1][0];
 
-    // If the user gives an answer, confirm it and let the loop run again
-    // If the user clicks okay without answering, prompt() returns "" empty string - consider same as cancel
-    // If the user clicks cancel, prompt() returns null, which breaks the loop and ends the game
-    if (userChoice === "") {
-        userChoice = null;
+    if (playerSelection === computerSelection) {
+        return "tie";
+    } else if ((playerSelection === "Rock" && computerSelection === "Scissors") ||
+        (playerSelection === "Paper" && computerSelection === "Rock") ||
+        (playerSelection === "Scissors" && computerSelection === "Paper")) {
+        //Increase player score
+        scores[0][1]++;
+        return "player";
     } else {
-        userChoice = confirmUserChoice(userChoice);
+        //Increase computer score
+        scores[1][1]++;
+        return "computer";
     }
 }
+
+function startGame(scores) {
+    displaySelection(scores);
+    getPlayerSelection(scores);
+}
+
+// [playerSelection, playerScore], 
+// [computerSelection, computerScore]
+let selectionsAndScores = [
+    ["", 0],
+    ["", 0]
+];
+
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+    startGame(selectionsAndScores);
+});
